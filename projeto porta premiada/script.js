46d1f5258fde9be2c8c78ccb400c5c83f468d341
laura flor <lauraflorrodrigues@gmail.com>
2026-04-16 16:31:43 -0300

    Add files via upload

--- projeto porta premiada/script.js
+++ projeto porta premiada/script.js
@@ -8,17 +8,23 @@ let texto = document.getElementById("mensagem")
 if (escolha === portaPremiada) {
         imagemClicada.src= "hogwarts.jpg"
       texto.innerText= "PARABÉNS! Você chegou a Hogwarts!"
+      document.getElementById("p1").removeAttribute("onclick")
+      document.getElementById("p3").removeAttribute("onclick")
         texto.style.color= "#2ecc71"
 
     } else if (escolha === 1) {
     imagemClicada.src= "carro.jpg"
         texto.innerText = "Você perdeu seu trem! Vá de carro voador!"
+        document.getElementById("p2").removeAttribute("onclick")
+        document.getElementById("p3").removeAttribute("onclick")
         texto.style.color = "#d81600"
     } 
 
  else {
         imagemClicada.src= "harryroom.png"
    texto.innerText = "Você perdeu seu trem! Tente ano que vem!"
+   document.getElementById("p2").removeAttribute("onclick")
+   document.getElementById("p1").removeAttribute("onclick")
         texto.style.color = "#eb1801"
     }
 
